929a42e3abe3ee8a50c5bd151fc49209afb10aac
daviddongguo <david.dong.guo@gmail.com>
2022-07-27 13:44:05 -0400

    webconfig is not defined

--- src/App.vue
+++ src/App.vue
@@ -3,21 +3,6 @@ import { onBeforeMount } from 'vue'
 import FloatingNavVue from './components/FloatingNav.vue'
 import ProfileVue from './components/Profile.vue'
 import RightVue from './components/Right.vue'
-onBeforeMount(() => {
-  const WebFontConfig = {
-    google: {
-      families: ['Roboto:300,400,700'],
-    },
-  }
-
-  ;(function (d) {
-    var wf = d.createElement('script'),
-      s = d.scripts[0]
-    wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js'
-    wf.async = true
-    s.parentNode.insertBefore(wf, s)
-  })(document)
-})
 </script>
 
 <template>
